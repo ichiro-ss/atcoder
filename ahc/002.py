b426c00a53c37e1f2378c7bd55ecbd5466dfb846
ichiro-ss <sei.swmr@gmail.com>
2022-05-12 23:48:12 +0900

    added ahc002

---
+++ ahc/002.py
@@ -0,0 +1,12 @@
+num = 50
+# input
+si, sj = [int(_) for _ in input().split()]
+t, p = [], []
+for _ in range(num):
+    t.append([int(_) for _ in input().split()])
+for _ in range(num):
+    p.append([int(_) for _ in input().split()])
+
+# solution1 print null string (4861 points)
+print()
+
