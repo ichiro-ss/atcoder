1dac1a6e4e9723c5797f3df675f4e2a5af731de7
ichiro-ss <sei.swmr@gmail.com>
2022-05-15 03:07:32 +0900

    solved abc251 d e

--- abc/251/d.py
+++ abc/251/d.py
@@ -1,3 +1,8 @@
 # input
 w = int(input())
 
+n = 99 * 3
+print(n)
+
+for i in range(1, 100):
+    print(i, i * (10 ** 2), i * (10 ** 4), end = " ")
--- abc/251/e.py
+++ abc/251/e.py
@@ -2,3 +2,19 @@
 n = int(input())
 a = [int(_) for _ in input().split()]
 
+INF = 10 ** 9 + 1
+
+dp = [[0 for _ in range(2)] for _ in range(n)]
+# without using a0
+dp[0][1] = INF
+for i in range(1, n):
+    dp[i][0] = dp[i-1][1]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][1]) + a[i]
+didnt_buy_4_1 = dp[n-1][1]
+
+# using a0
+dp[0][0] = INF; dp[0][1] = a[0]
+for i in range(1, n):
+    dp[i][0] = dp[i-1][1]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][1]) + a[i]
+print(min(min(dp[n-1][0], dp[n-1][1]), didnt_buy_4_1))
