1e2e6dcef25a223a8dfeec8f8c980f0300f60754
ichiro-ss <sei.swmr@gmail.com>
2022-05-16 13:26:49 +0900

    solved ahc002 dfs 2sec

--- ahc/002.py
+++ ahc/002.py
@@ -1,18 +1,16 @@
+import time
+from random import shuffle
 import sys
+st = time.time()
 sys.setrecursionlimit(10 ** 9)
 num = 50
+
 # input
 si, sj = [int(_) for _ in input().split()]
-t, p = [], []
-for _ in range(num):
-    t.append([int(_) for _ in input().split()])
-for _ in range(num):
-    p.append([int(_) for _ in input().split()])
+t = [[int(_) for _ in input().split()] for _ in range(num)]
+p = [[int(_) for _ in input().split()] for _ in range(num)]
 
-# solution1 print null string (4861 points)
-# print()
-
-# solution2 simple dfs
+# solution3 dfs till end
 dh = [-1, 1, 0, 0]
 dw = [0, 0, -1, 1]
 dir = ["U", "D", "L", "R"]
@@ -20,10 +18,18 @@ dir = ["U", "D", "L", "R"]
 ans_way = ""
 max_score = 0
 def dfs(pr_h, pr_w, so_far, score, passed):
-    global max_score, ans_way
+    global max_score, ans_way, st
+
+    if (time.time() - st) > 1.8:
+        if score > max_score:
+            max_score = score
+            ans_way = so_far
+        return
 
     passed[t[pr_h][pr_w]] = True
-    for i in range(4):
+    idxs = list(range(4))
+    shuffle(idxs)
+    for i in idxs:
         nx_h, nx_w = pr_h + dh[i], pr_w + dw[i]
         if 0 <= nx_h < num and 0 <= nx_w < num and not passed[t[nx_h][nx_w]]:
             nx_score = score + p[nx_h][nx_w]
@@ -34,5 +40,8 @@ def dfs(pr_h, pr_w, so_far, score, passed):
         max_score = score
         ans_way = so_far
 
-dfs(si, sj, "", p[si][sj], [False] * (num * num))
+while True:
+    dfs(si, sj, "", p[si][sj], [False] * (num * num))
+    if (time.time() - st) > 1.8:
+        break
 print(ans_way)
