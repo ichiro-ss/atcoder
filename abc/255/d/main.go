c918bec49534eb369c6ff2c06cb59e94d71c2631
ichiro-ss <sei.swmr@gmail.com>
2023-09-24 03:02:57 +0900

    solved 255/d

--- abc/255/d/main.go
+++ abc/255/d/main.go
@@ -1,39 +1,48 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"math"
+	"io"
+	"os"
+	"sort"
 )
 
-func main() {
-	var n, k int
-	fmt.Scanf("%d %d", &n, &k)
-	var a []int
-	var have_light [100000]bool
+func run(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
 
-	for i := 0; i < k; i++ {
-		var x int
-		fmt.Scanf("%d", &x)
-		a = append(a, x-1)
-		have_light[x-1] = true
+	var n, q int
+	fmt.Fscan(in, &n, &q)
+	a := make([]int, n)
+	for i := range a {
+		fmt.Fscan(in, &a[i])
 	}
-	var x, y []int
-	for i := 0; i < n; i++ {
-		var b, c int
-		fmt.Scanf("%d %d", &b, &c)
-		x = append(x, b)
-		y = append(y, c)
+	sort.Ints(a)
+
+	asum := make([]int64, len(a))
+	asum[0] = int64(a[0])
+	for i := 1; i < n; i++ {
+		asum[i] = asum[i-1] + int64(a[i])
 	}
 
-	max := 0.0
-	var dist float64
-	for i := 0; i < n; i++ {
-		min := math.Pow10(12)
-		for j := 0; j < k; j++ {
-			dist = math.Pow(float64(x[a[j]]-x[i]), 2.0) + math.Pow(float64(y[a[j]]-y[i]), 2.0)
-			min = math.Min(min, dist)
+	var k int
+	var res int64
+	var x int
+	for i := 0; i < q; i++ {
+		fmt.Fscan(in, &x)
+		k = sort.SearchInts(a, x)
+		if k >= len(asum) {
+			res = int64(k*x) - asum[k-1]
+		} else if k == 0 {
+			res = asum[len(asum)-1] - int64(len(asum)*x)
+		} else {
+			res = int64(k*x) - asum[k-1] + asum[len(asum)-1] - asum[k-1] - int64((len(asum)-k)*x)
+			// fmt.Println("k:", k, "--", k*x[i], "-", asum[k], "+", asum[len(asum)-1], "-", asum[k-1], "-", k*x[i])
 		}
-		max = math.Max(max, min)
+		fmt.Fprintln(out, res)
 	}
-	fmt.Println(math.Sqrt(max))
 }
+
+func main() { run(os.Stdin, os.Stdout) }
